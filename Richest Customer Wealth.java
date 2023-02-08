8f94c41f6433575de40469aab187244802fa737b
Ratan Gulati <i.ratangulati@gmail.com>
2023-02-08 14:00:09 +0530

    Update Richest Customer Wealth.java

--- Richest Customer Wealth.java
+++ Richest Customer Wealth.java
@@ -7,7 +7,6 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
         int ans = 0;
         for (var e : accounts) {
-            // int s = Arrays.stream(e).sum();
             int s = 0;
             for (int v : e) {
                 s += v;
